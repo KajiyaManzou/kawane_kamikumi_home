f671c81cb494c3953dca11b639808e5cea72a72a
Kajiya Manzou <kajiya.coffee@gmail.com>
2026-04-04 09:23:12 +0000

    更新日付を変更

--- src/_data/tokai_news.js
+++ src/_data/tokai_news.js
@@ -4,6 +4,9 @@ const Parser = require("rss-parser");
 const RSS_URL = "https://www.vill.tokai.ibaraki.jp/cgi-bin/feed.php?siteNew=1";
 
 module.exports = async function () {
+  const processedAt = new Date();
+  const processedDateJa = (processedAt.getMonth() + 1) + "月" + processedAt.getDate() + "日";
+
   try {
     const xml = await EleventyFetch(RSS_URL, {
       duration: "1h",
@@ -13,7 +16,7 @@ module.exports = async function () {
     const parser = new Parser();
     const feed = await parser.parseString(xml);
 
-    return feed.items.slice(0, 5).map((item) => {
+    const items = feed.items.slice(0, 5).map((item) => {
       const d = item.date ? new Date(item.date) : null;
       const dateJa = d
         ? `${d.getMonth() + 1}月${d.getDate()}日`
@@ -24,8 +27,16 @@ module.exports = async function () {
         dateJa,
       };
     });
+
+    return {
+      items,
+      processedDateJa,
+    };
   } catch (e) {
     console.warn("[tokai_news] RSS取得失敗:", e.message);
-    return [];
+    return {
+      items: [],
+      processedDateJa,
+    };
   }
 };
